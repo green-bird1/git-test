f80d8e6b22774b7a5edc03ccb719b7a5da5717ab
greenbird <3582033455@qq.com>
2023-03-02 21:34:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author greenbird
+ * @create 2023-03-02 21:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello GitTest");
+    }
+}
